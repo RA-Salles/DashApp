056626340585330595fff6e3cb3f147341a4d646
RA-Salles <107194597+RA-Salles@users.noreply.github.com>
2022-07-05 17:56:14 -0300

    Done some more testing with callbacks
    Will soon finish them. And, when I do, I'll be able to do some page changing inside the app, which would be quite nice. 
    As you can see, there's now some @app.callbacks there. I've yet to wrap my head around their innerworking, but first, gotta make this
    front-end somewhat functional. Fixed the mola thingy too, although its behaviour is not quite there yet. Must see what the heck is making the particles accelerate so damn fast. Even with acceleration forced to zero, leaving leeway for the spring class to work, they are bouncing around increasingly. Pain.

--- dashapp2.py
+++ dashapp2.py
@@ -7,9 +7,7 @@ Created on Fri Jul  1 15:16:10 2022
 """
 
 import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Output, Input
+from dash import dcc, html, Output, Input
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=([dbc.themes.BOOTSTRAP]))
@@ -23,17 +21,17 @@ title = dbc.Row([
 navbar = dbc.Row([ 
     dbc.NavbarSimple(
         children=[
-            dbc.NavItem(dbc.NavLink("Simulações", href='#')),
-            dbc.NavItem(dbc.NavLink("Implicações", href='#')),
-            dbc.NavItem(dbc.NavLink("Satisfações", href='#'))],
+            dbc.NavItem(dbc.NavLink("Simulações" , href='/sim', id = 'simulacro' )),
+            dbc.NavItem(dbc.NavLink("Implicações", href='/imp', id = 'implicacro')),
+            dbc.NavItem(dbc.NavLink("Satisfações", href='/docs', id = 'satisfacro'))
+            ],
         brand = "Colisor: Uma Simulação Subjetivamente Simples",
         brand_href="#",
         color="primary",
         dark=True
         )
     ])
-contentbase = dbc.Row([ #Isso aqui é a coisa que vai ser trocada pelos page_content
-    ])
+contentbase = html.Div(id='test')
 page_content_simulacoes = dbc.Row([ #Adicionar dropdown para seleção de tipo de animação, etc.
     dbc.Col([dcc.Graph
              ])
@@ -52,13 +50,51 @@ page_content_implicacoes = dbc.Container([
             ])
         ])
     ])
-#page_content_satisfacoes = #INSERIR AQUI ESTURUTRA DE EXPLICAÇÃO DAS FUNÇÕES
+page_content_satisfacoes = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1("Cacetada, isso é um título"), 
+            html.H3("e isso, um subtítulo!")
+            ])
+        ]),
+    dbc.Row([
+        dbc.Col([
+            html.H5(" e isso aqui deveria conter algumas \
+                    explicações acerca da documentação das classes\
+                        e objetos criados no código")
+            ])
+        ])
+    ])
 
 app.layout = dbc.Container([ 
     title,
     navbar,
-    page_content_implicacoes
+    
     ])
+
+@app.callback(
+    Output(component_id = 'test', component_property = 'children'),
+    Input (component_id = 'simulacro', component_property = 'n_clicks')
+    )
+
+def displaysim(n_clicks):
+    return page_content_simulacoes
+
+@app.callback(
+    Output(component_id = 'test', component_property = 'children'),
+    Input (component_id = 'implicacro', component_property = 'n_clicks')
+    )
+
+def displayimp(n_clicks):
+    return page_content_implicacoes
+
+@app.callback(
+    Output(component_id = 'test', component_property = 'children'),
+    Input (component_id = 'satisfacro', component_property = 'n_clicks')
+    )
+
+def displaysats(n_clicks):
+    return page_content_satisfacoes
 
 if __name__ == '__main__':
     app.run_server(debug=True)
